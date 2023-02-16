4c3b22bace10a4e6b6d2a72083ec5ad2c9714edd
Aleks Petkov <aleksp@kth.se>
2023-02-16 14:38:57 +0100

    Finish centering pieces on squares, begin verifying piece is moved from correct square

--- client/src/components/Chessboard/Chessboard.tsx
+++ client/src/components/Chessboard/Chessboard.tsx
@@ -11,12 +11,16 @@ const SQUARE_SIZE = 100;
 let grabbedElement: HTMLElement | null = null
 let grabbedPiece: Piece | null = null
 
-const fileNumberToString = (file: number): string => {
+const getFileString = (file: number): string => {
   return files[file - 1]
 }
 
+const getFileNumber = (file: string): number => {
+  return files.indexOf(file) + 1
+}
+
 export const pieceToSymbol = (piece: Piece | null): string | undefined => {
-  return piece?.color === 'w' ? piece.type.toUpperCase() : piece?.type
+  return piece?.color === 'w' ? piece?.type.toUpperCase() : piece?.type
 }
 
 
@@ -24,17 +28,54 @@ const isPiece = (e: HTMLElement): boolean => {
   return e.classList.contains('piece')
 }
 
+// TODO: decide what functions to put inside component
 const Chessboard = () => {
+
+  const [fromSquare, setFromSquare] = useState("")
+  const [file, setFile] = useState(0)
+  const [rank, setRank] = useState(0)
+
+  const centerOfSquare = (file: string, rank: number): number[] => {
+    const chessboard = chessRef.current!
+    const centerX = getFileNumber(file) * SQUARE_SIZE
+    const centerY = rank * SQUARE_SIZE
+    const minX = chessboard.offsetLeft - OFFSET
+    const maxY = chessboard.offsetTop + chessboard.clientHeight + OFFSET
+    return [minX + centerX, maxY - centerY]
+  }
+
+  // TODO: confirm grabedPiece starting position == chess.move.from 
+  // to discard e.g. moving e-pawn to f-file
+  const makeMove = (fromSquare: string, toSquare: string): void => {
+    if (grabbedElement === null) return;
+    console.log(grabbedPiece)
+    console.log("FIle: " + file + "rank: " + rank)
+    const move = getSAN(grabbedPiece, toSquare) //grabbedPiece never null
+    console.log("Move: " + move)
+    const b = chess.move(move)
+    console.log(b)
+    const center = centerOfSquare(toSquare[0], parseInt(toSquare[1]))
+    moveElement(grabbedElement, center[0], center[1])
+  }
+
 
   const squareOnCursor = (x: number, y: number): string => {
     const chessboard = chessRef.current!
     const file = Math.floor((x - chessboard.offsetLeft) / SQUARE_SIZE) + 1
     const rank = Math.abs(Math.ceil((y - chessboard.offsetTop - GRID_SIZE) / SQUARE_SIZE)) + 1
-    return fileNumberToString(file) + rank.toString()
+    setFromSquare(getFileString(file) + rank.toString())
+    console.log("Fromsquare: " + fromSquare)
+    return fromSquare
   }
 
   const pieceOnCursor = (x: number, y: number): Piece | null => {
     return chess.get(squareOnCursor(x, y))
+  }
+
+  const getSAN = (piece: Piece | null, square: string): string => {
+    let move = pieceToSymbol(piece) + square; //grabbedPiece never null
+    move = move[0].toLowerCase() === "p" ? move.substring(1) : move;
+    return move;
   }
 
   const grabPiece = (e: React.MouseEvent): void => {
@@ -52,17 +93,14 @@ const Chessboard = () => {
     }
   }
 
+
   const dropPiece = (e: React.MouseEvent): void => {
     const maybePiece: Piece | null = pieceOnCursor(e.clientX, e.clientY)
     console.log(maybePiece)
     const chessboard = chessRef.current!
-    if (maybePiece === null && grabbedElement !== null) {
-      const square = squareOnCursor(e.clientX, e.clientY);
-      let move = pieceToSymbol(grabbedPiece) + square; //grabbedPiece never null
-      move = move[0].toLowerCase() === "p" ? move.substring(1) : move;
-      console.log(move)
-      const b = chess.move(move)
-      console.log(b)
+    if (maybePiece === null) {
+      const toSquare = squareOnCursor(e.clientX, e.clientY);
+      makeMove("", toSquare)
       grabbedElement = null
     }
   }
